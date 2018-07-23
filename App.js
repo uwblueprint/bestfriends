a8f729ede359038481d85fd2b18b0d9af7a601ef
Narayan <narayangupta763@gmail.com>
2018-07-23 17:52:33 -0400

    Componentised camrea

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import CameraExample from './Camera';
 
 export default class BestFriendsApp extends React.Component {
 
   render() {
-    
+    return(<CameraExample></CameraExample>)
   }
   
 }
