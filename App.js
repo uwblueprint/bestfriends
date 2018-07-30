e23f867c3547f9670d5a7cbadf37b0cc76471a1b
Narayan <narayangupta763@gmail.com>
2018-07-30 09:43:14 -0400

    Styling added to the on boarding screens -Howard

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import CameraExample from './Camera';
+import Onboarding from './Onboarding';
+import {
+  creatStackNavigator,
+  
+} from 'react-navigation';
+
+const navigator = creatStackNavigator({
+  Home:{
+    screen: CameraExample
+  },
+  
+})
 
 export default class BestFriendsApp extends React.Component {
 
   render() {
-    return(<CameraExample></CameraExample>)
+    return <navigator/>
   }
   
 }
+
+
