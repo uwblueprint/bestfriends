f1515018ec07025099d59c84b56f982a06fa8986
Narayan <narayangupta763@gmail.com>
2018-09-08 15:24:44 -0400

    Updated Read me, fixed compilation issues

--- App.js
+++ App.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import CameraExample from './Camera';
 import Onboarding from './Onboarding';
-import {
-  creatStackNavigator,
-  
-} from 'react-navigation';
 
-const navigator = creatStackNavigator({
-  Home:{
-    screen: CameraExample
-  },
-  
-})
+
+
 
 export default class BestFriendsApp extends React.Component {
 
   render() {
-    return <navigator/>
+    return <Onboarding></Onboarding>
   }
   
 }
