4bf41ab7c4689d5c6008e732909e573154675f30
jtaaa <joshuatallum@gmail.com>
2018-11-15 15:33:15 -0500

    Add onboarding screen, resources and logic

--- App.js
+++ App.js
@@ -1,25 +1,43 @@
 import React from 'react';
+import { AsyncStorage } from 'react-native';
 import CameraExample from './Camera';
 import Onboarding from './Onboarding';
 
 export default class BestFriendsApp extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.onOnboardingComplete = this.onOnboardingComplete.bind(this);
+  }
+
   state = {
-    screen: 'onboarding',
+    onboarded: null,
   };
 
-  openCamera = () => {
-    this.setState({screen: 'camera'})
+  componentDidMount() {
+    AsyncStorage.getItem('alreadyLaunched').then(value => {
+      if (value == null) {
+        AsyncStorage.setItem('alreadyLaunched', 'true');
+        this.setState({ onboarded: false });
+      } else {
+        this.setState({ onboarded: true });
+      }
+    });
+  }
+
+  onOnboardingComplete() {
+    this.setState({ onboarded: true });
   }
 
   render() {
-    return (
-      this.state.screen == 'onboarding' ? 
-      <Onboarding openCamera={this.openCamera}></Onboarding> : 
-      <CameraExample></CameraExample>
-    );
-      
+    if (this.state.onboarded === null) {
+      return null;
+    } else if (this.state.onboarded === false) {
+      return <Onboarding onComplete={() => this.onOnboardingComplete()}></Onboarding>;
+    } else {
+      return <CameraExample></CameraExample>;
+    }
   }
-  
 }
 
 
--- Onboarding.js
+++ Onboarding.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 import{
 	AppRegistry, // Registers the App
 	StatusBar, //Allows us the hide the status bar
+	Image,
+	View,
+	Text,
+	StyleSheet,
 } from 'react-native'
-
-import Screens from './Screens';
+import Button from './Button';
 
 export default class Onboarding extends Component {
 	compondnetDidMount(){
@@ -12,9 +15,35 @@ export default class Onboarding extends Component {
 	}
 	render(){
 		return(
-			<Screens openCamera={this.props.openCamera}/>
+			<View style={styles.obContainer}>
+				<Image style={styles.obImage} source={require('./onboarding.png')}/>
+				<Text style={styles.obText}>Get started to begin taking amazing photos for the pets!</Text>
+				<Button text="Get Started" onPress={this.props.onComplete}></Button>
+			</View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	obContainer: {
+		flex: 1,
+		flexDirection: 'column',
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: '#FA770B',
+		paddingHorizontal: 60,
+		paddingBottom: 30,
+	},
+	obText: {
+		fontSize: 24,
+		color: 'white',
+		letterSpacing: 1.18,
+		marginBottom: 57,
+	},
+	obImage: {
+		width: 270,
+		height: 270,
+	},
+});
 
 AppRegistry.registerComponent('Onboarding', () => Onboarding);
