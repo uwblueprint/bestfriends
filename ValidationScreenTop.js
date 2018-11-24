6e1e98c73b6539f8ff28cf9cd5fcbdd5db709802
Narayan <narayangupta763@gmail.com>
2018-11-24 13:46:43 -0500

    Base content for validation screen top

---
+++ ValidationScreenTop.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {Image, View, StyleSheet, Text} from 'react-native';
+import { Icon } from 'react-native-elements'
+export default class ValidationScreenTop extends React.Component {
+  constructor(props){
+      super(props)
+  }
+
+  
+
+  render() {
+    return (
+    <View>
+        <View>
+            <Icon name = "cancel"/>
+        </View>
+            <Text>Save Your Photos</Text>
+        <View style = {styles.circle}>
+            <Text style = {styles.textStyle} >4</Text>
+        </View>
+    </View>
+      
+    );
+      
+  }
+  
+}
+const styles = StyleSheet.create({
+    // Slide styles
+    circle: {
+        height: 30,
+        width: 30,
+        borderRadius: 30,
+        backgroundColor: '#ffa500',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      textStyle:{
+        color:  '#FFFFFF'
+
+      }
+   
+   });
